beecd2d13d267b9dbd6c524ebc1e595beaa95f04
TianChenjiang <tiancj0310@outlook.com>
2020-01-14 20:20:04 +0800

    add #32

--- C++/32.longest-valid-parentheses.cpp
+++ C++/32.longest-valid-parentheses.cpp
@@ -8,7 +8,23 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        
+         stack<int> st;
+        st.push(-1);
+        int res = 0;
+        int cur = 0;
+        for (int i = 0; i < s.size(); i++) {
+            char chr = s[i];
+            if (chr == '(') st.push(i);
+            else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);//新的开始
+                } else {
+                    res = max(res, i-st.top());
+                }   
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
