76c1ab34462618d9804680f7cb4018262ab5f4fd
TianChenjiang <tiancj0310@outlook.com>
2020-12-29 16:50:10 +0800

    update stock problem

--- C++/188.cpp
+++ C++/188.cpp
@@ -4,20 +4,14 @@ public:
         if (prices.empty()) return 0;
         int n = prices.size();
         k = min(k, n/2);
-        vector<int> sell(n+1), buy(n+1);
-        
-        sell[0] = 0, buy[0] = -prices[0];
-        for (int i = 1; i <= k; ++i) {
-            buy[i] = sell[i] = INT_MIN / 2;
-        }
+        vector<int> sell(k+1), buy(k+1, INT_MIN);
 
-        for (int i = 1; i < n; i++) {
-            buy[0] = max(buy[0], sell[0] - prices[i]);
-            for (int j = 1; j <= k; j++) {
-                sell[j] = max(sell[j], buy[j-1] + prices[i]);
-                buy[j] = max(buy[j], sell[j] - prices[i]);
+        for (int price : prices) {
+            for (int i = 1; i <= k; i++) {
+                buy[i] = max(buy[i], sell[i-1] - price);
+                sell[i] = max(sell[i], buy[i] + price);
             }
         }
-        return *max_element(sell.begin(), sell.end());
+        return sell[k];
     }
 };
