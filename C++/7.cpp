0acc02d9c1a1546da0bbbfcca2b2afaf8da5a558
TianChenjiang <tiancj0310@outlook.com>
2021-05-03 14:05:12 +0800

    add 7

---
+++ C++/7.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return int(res) == res ? res : 0;
+    }
+};
